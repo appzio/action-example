18b829c485adc21bc4e2ce33df0d4f0b3167c524
Timo Railo <timo@appzio.com>
2018-12-20 15:11:12 +0200

    migrations example updated (NOTE: requires the latest Docker container)

--- Migrations/MigrationsactionMexample.php
+++ Migrations/MigrationsactionMexample.php
@@ -1,6 +1,7 @@
 <?php
 
 Yii::import('application.modules.aeapi.models.*');
+Yii::import('application.modules.aelogic.Bootstrap.Models.*');
 
 /*
  * This runs any custom migrations needed by the action module. These migrations
@@ -31,45 +32,22 @@ Yii::import('application.modules.aeapi.models.*');
  *
  */
 
-class MigrationsactionMexample extends Migrations {
+class MigrationsactionMexample extends BootstrapMigrations {
 
-    public static function runModuleMigrations(){
-        self::createExampleAction();
+    /* set these so that the action will get created automatically */
+    public $title = 'Mobile Example';
+    public $icon = 'new.png';
+    public $description = 'Example module';
+
+    public function runModuleMigrations(){
+        $this->exampleMigration();
         return true;
     }
 
-    private static function createExampleAction()
-    {
-        if(self::helperActionExists('mexample')){
-            return false;
-        }
-
-        $sql = "
-          INSERT INTO `ae_game_branch_action_type` (`title`, `icon`, `shortname`, `id_user`, `description`, `version`, `channels`, `uiformat`, `active`, `global`, `githubrepo`, `adminfeedback`, `requestupdate`, `uses_table`, `has_statistics`, `has_export`, `invisible`, `hide_from_api`, `ios_supports`, `android_supports`, `web_supports`, `article_view`,`library`) VALUES
-          ('Mobile Example', 'new.png', 'mexample', 1, '<p>Example module</p>', '1', '', 'native', 1, 1, '', '', 0, 0, 0, 0, 0, 0, 1, 1, 0, 1,'PHP2');
-        ";
-
-        @Yii::app()->db->createCommand($sql)->query();
-    }
-
-
-    private static function exampleMigration(){
-        if(!self::helperTableExists('ae_ext_example')) {
-            $path = Yii::getPathOfAlias('application.modules.aelogic.packages.actionMexample.Migrations');
-            $sql = file_get_contents($path . DS . 'tables.sql');
-            @Yii::app()->db->createCommand($sql)->query();
-        }
-    }
-
-    private static function exampleAppSpecific(){
-        // this will run only if its executed from the admin dashboard, not automatically on pod deployment
-        if(isset($_REQUEST['gid'])){
-            if(!self::helperTableExists('ae_ext_example')){
-                $path = Yii::getPathOfAlias('application.modules.aelogic.packages.actionMexample.Migrations');
-                $sql = file_get_contents($path.DS.'tables.sql');
-                $sql = str_replace('{{app_id}}', $_REQUEST['gid'], $sql);
-                @Yii::app()->db->createCommand($sql)->query();
-            }
+    /* example on how to run an external sql file */
+    private function exampleMigration(){
+        if(self::helperTableExists('ae_game')){
+            $this->runMigrationFromFile('tables.sql');
         }
     }
 
